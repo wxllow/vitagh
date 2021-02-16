c37807bce90018f31784b0a8cd3092ff30baee9a
Willow <willowjl1117@gmail.com>
2021-02-15 19:15:31 -0500

    Fix images (pngquant)

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@
 int main(int argc, char *argv[]) {
 	std::stringstream output;
 	std::vector<std::string> hello = { "Hello" };
-	hello.push_back(",");
-	hello.push_back(" C++ ");
 	hello.push_back("world!");
 	for (auto &s : hello) {
 		// std::cout does't work ATM :(
